7d8f5d3fcb70b478ffb1f2ce6c1f1bf7001abb39
Yusuke Wada <yusuke@kamawada.com>
2022-02-17 22:34:35 +0900

    update hono

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
+import { poweredBy } from 'hono/powered-by'
 
 const app = new Hono()
+
+app.use('*', poweredBy())
 app.get('/', (c) => c.html('<h1>Hello Hono!</h1>'))
 
 app.fire()
