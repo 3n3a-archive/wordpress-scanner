fc0b3241b2d15734632318131ce765f3fee28515
3n3a <46775561+3n3a@users.noreply.github.com>
2022-09-08 17:30:10 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,15 @@ const app = new Hono()
 
 // Builtin middleware
 app.use('*', poweredBy())
-// Basic Auth
-app.use(
-  '/auth/*',
-  basicAuth({
-    username: 'hono',
-    password: 'acoolproject',
-  })
-)
 
 // Custom middleware
 app.use('*', async (c, next) => {
   await next()
-  c.header('X-message', 'Hono is a cool project')
+  c.header('X-message', 'Scan da wordpress')
 })
 
 // Routing
-app.get('/', (c) => c.html('<h1>Hello Hono!</h1>'))
-app.get('/auth/*', (c) => c.text('You are authorized'))
+app.get('/', (c) => c.html('<h1>Hello WP!</h1>'))
 
-// Nested route
-const book = new Hono()
-// Named path parameters
-book.get('/:id', (c) => {
-  const id = c.req.param('id')
-  return c.json({ 'Your book ID is': id })
-})
-book.post('/', (c) => c.text('Book is created', 201))
-app.route('/book', book)
 
 export default app
