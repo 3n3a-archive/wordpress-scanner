080fb21c5a11fec9c39da30d006da1cc1c3edc91
3n3a <46775561+3n3a@users.noreply.github.com>
2022-09-09 07:46:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ class ScriptTagHandler {
   }
   element(e) {
     const src = e.getAttribute(this.attr)
-    if(src.contains('ver')) versions.push(src)
+    if(src.indexOf('ver') != -1) versions.push(src)
   }
 }
 
