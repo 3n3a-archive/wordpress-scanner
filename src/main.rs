2f8227fa68d5cab7e721a5f0010a16f41bd7cf56
3n3a <info@3n3a.ch>
2022-09-17 18:18:08 +0200

    add frontend (simple) and api with dynamic input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 #[macro_use]
 extern crate rocket;
+use rocket_dyn_templates::{Template, context};
 
 mod requestor;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![index])
+        .mount("/scan", routes![scan_site])
+        .attach(Template::fairing())
 }
 
 #[get("/")]
-async fn hello() -> String {
-    let text = requestor::get_site().await.unwrap();
+fn index() -> Template {
+    let context = context! {
+        title: "Wordpress Scanner"
+    };
+    Template::render("index", &context)
+}
+
+#[get("/<url>")]
+async fn scan_site(url: &str) -> String {
+    let text = requestor::get_site(url).await.unwrap();
     text
 }
--- src/requestor.rs
+++ src/requestor.rs
@@ -1,8 +1,8 @@
 use reqwest;
 
-pub async fn get_site() -> Result<String, Box<dyn std::error::Error>> {
+pub async fn get_site(url: &str) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let result = client.get("https://httpbin.org/get")
+    let result = client.get(url)
         .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
         .send()
         .await?;
